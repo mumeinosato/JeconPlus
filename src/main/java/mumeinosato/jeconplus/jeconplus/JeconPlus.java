7b2fe9fc07c7ffc23633f9ed0bd872df5dc9c69a
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-08 20:56:19 +0900

    Initial commit

---
+++ src/main/java/mumeinosato/jeconplus/jeconplus/JeconPlus.java
@@ -0,0 +1,17 @@
+package mumeinosato.jeconplus.jeconplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JeconPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
